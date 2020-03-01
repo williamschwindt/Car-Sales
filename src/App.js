992b4e4fada8826ec3e5e938481d667a01904747
William Schwindt <williammschwindt@gmail.com>
2020-03-01 15:42:35 -0800

    store and provider set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { createStore } from 'redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+
+import { rootReducer } from './reducers/index';
+
+export const store = createStore(rootReducer);
 
 const App = () => {
   const state = {}
